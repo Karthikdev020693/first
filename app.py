2bcd4a5f4aa06bac75ee4cb83cd72b2656e00ff4
zeternum <zeternum@zeternums-MacBook-Air.local>
2024-10-12 14:38:37 +0530

    second commit

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-test
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Flask app on Docker!"
+
+if __name__ == '__main__':
+        app.run(host='0.0.0.0')
